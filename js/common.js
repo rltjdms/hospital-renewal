43f2ce01e3c89acc4246b6040c4dbce47c75281a
rltjdms <137126199+rltjdms@users.noreply.github.com>
2026-06-17 15:54:15 +0900

    [Add] Nav code

--- js/common.js
+++ js/common.js
@@ -1,11 +1,26 @@
 const header = document.querySelector('.head_inner');
 const nav = document.querySelector('nav');
 const nav_back = document.querySelector('.gnb_bg');
+const menuItems = document.querySelectorAll('.main_menu > li');
 
-nav.addEventListener('mouseenter', () => {
-    header.classList.add('on');
+[nav, nav_back].forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        header.classList.add('on');
+    });
+
+    el.addEventListener('mouseleave', () => {
+        header.classList.remove('on');
+    });
 });
 
-nav_back.addEventListener('mouseleave', () => {
-    header.classList.remove('on');
+menuItems.forEach((item) => {
+
+  item.addEventListener('mouseenter', () => {
+    item.classList.add('active');
+  });
+
+  item.addEventListener('mouseleave', () => {
+    item.classList.remove('active');
+  });
+
 });
